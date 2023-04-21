0f82a963458d1ffa60bffcc74ecaf58e7d21662f
Sarah Nadeau <san62@cornell.edu>
2023-04-21 15:06:50 -0700

    make figs including total cluster sizes

---
+++ nextflow/bin/get_node_to_leaves_mat.py
@@ -0,0 +1,38 @@
+#!python3
+
+import argparse
+import sys
+import pandas as pd
+import bte
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--mat_pb", help="MAT protobuf file", type=str, 
+    default="../matutils/mutation_annotated_tree.pb")
+parser.add_argument(
+    "--introductions", help="Introductions file", type=str,
+    default="introductions.tsv")
+args = parser.parse_args()
+
+# Load data
+mat = bte.MATree(args.mat_pb)
+introductions = pd.read_table(args.introductions)
+
+# Get introduction nodes
+introduction_nodes = []
+for index, row in introductions.iterrows():
+    introduction_nodes.append(row['introduction_node'])
+
+# Get the leaves for each introduction node in tree
+leaf_data = pd.DataFrame(columns=['introduction_node', 'leaf'])
+
+for introduction_node in set(introduction_nodes):
+    node_id = introduction_node.split("_", 1)[1]
+    leaf_list = mat.get_leaves_ids(nid=node_id)
+    print(str(introduction_node) + " has " + str(len(leaf_list)) + " leaves")
+    list_of_tuples = list(zip([introduction_node] * len(leaf_list), leaf_list))
+    leaf_data_tmp = pd.DataFrame(list_of_tuples, columns = ['introduction_node', 'leaf'])
+    leaf_data = pd.concat([leaf_data, leaf_data_tmp], ignore_index=True)
+
+leaf_data.to_csv("node_to_leaves.csv", index=False)
