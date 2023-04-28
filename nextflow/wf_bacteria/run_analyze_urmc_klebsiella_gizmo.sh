ec5f1a21a3baefe337161cdc908d5f54f8bd3ea5
Sarah Nadeau <san62@cornell.edu>
2023-04-28 11:27:27 -0700

    update urmc analysis after refactor

--- nextflow/wf_bacteria/run_analyze_urmc_klebsiella_gizmo.sh
+++ nextflow/wf_bacteria/run_analyze_urmc_klebsiella_gizmo.sh
@@ -8,12 +8,17 @@ ml nextflow/23.04.0
 ml Apptainer/1.1.6
 
 # Run the workflow
-echo "WARNING: -stub-run is on!"
 nextflow \
     run \
     -c ../nextflow.gizmo.notower.config \
     -N snadeau@fredhutch.org \
-    analyze_urmc_klebsiella.nf \
+    analyze_clusters_bacteria.nf \
     -profile apptainer \
+    --input_fasta_dir "../../clean_data/klebsiella_malek_urmc/all_samples_for_analysis" \
+    --input_metadata "../../clean_data/klebsiella_malek_urmc/clustertracker_metadata.txt" \
+    --input_metadata_nextstrain "../../clean_data/klebsiella_malek_urmc/nextstrain_metadata.csv" \
+    --reference_fasta "../../clean_data/klebsiella_malek_urmc/reference_NC_015663v1.fna" \
+    --trait_name "location" \
+    --nextstrain_refine_params --coalescent opt --root reference.fasta.ref \
     -resume \
     -stub-run
