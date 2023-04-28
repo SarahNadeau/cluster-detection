049a7408f8167be8e9bc1c94eef815da3e8a6c0a
Sarah Nadeau <san62@cornell.edu>
2023-04-28 11:19:28 -0700

    update boston analysis after refactor

--- nextflow/wf_sars_cov_2/run_analyze_boston_confa_gizmo_bigcontext.sh
+++ nextflow/wf_sars_cov_2/run_analyze_boston_confa_gizmo_bigcontext.sh
@@ -12,13 +12,19 @@ ml Apptainer/1.1.6
 export AUGUR_RECURSION_LIMIT=10000
 
 # Run the workflow
-echo "WARNING: stub-run is on!"
 nextflow \
     run \
     -c ../nextflow.gizmo.notower.config \
     -N snadeau@fredhutch.org \
-    analyze_boston_confa.nf \
+    analyze_clusters_sars_cov_2.nf \
     -profile apptainer \
+    --input_fasta "../../clean_data/sars_cov_2_lemiux_boston/outbreak_samples_confa.fasta" \
+    --input_metadata "../../clean_data/sars_cov_2_lemiux_boston/clustertracker_metadata.txt" \
+    --input_metadata_nextstrain "../../clean_data/sars_cov_2_lemiux_boston/nextstrain_metadata.txt" \
+    --trait_name "division" \
     --context_region_name '.' \
-    -resume \
-    -stub-run 
+    --reference_fasta "../../clean_data/sars_cov_2_lemiux_boston/reference_NC_045512v2.fa" \
+    --filter_similarity_specs "--min-length 20000 --max-date '2020-07-01' --subsample-max-sequences 100" \
+    --filter_geocontext_specs "--min-length 20000 --max-date '2020-07-01' --group-by region year month --subsample-max-sequences 200" \
+    --outgroup_taxon "NC_045512v2" \
+    --nextstrain_refine_params "--coalescent opt --date-confidence --clock-rate 0.0008 --clock-std-dev 0.004 --root NC_045512v2"
